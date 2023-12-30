e1b721c7fab6dc3e4b3a7a16acf609b2b8e8d094
Adithyasid <129668862+Adithyasid@users.noreply.github.com>
2023-12-30 21:13:56 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,10 @@
+let testDiv = document.getElementById("#pp");
+function HideCursor() {
+         
+     // hiding the cursor
+    testDiv.style.cursor = "none";
+}
+function showAllert(){
+    var myText="ITS FUNNY U THINK U HAVE AN OPTION. \n CLICK 'YES' \nAM I CLEAR?";
+    alert(myText);
+}
